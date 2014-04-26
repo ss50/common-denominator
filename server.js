7491ee6ac79189c375cd18e5ae182cbc17e8ae62
asvarga <alexander_varga@brown.edu>
2014-04-26 11:34:00 -0400

    deleted server.js, renamed barbones to server

--- server.js
+++ server.js
@@ -1,20 +1,11 @@
-
-function c(x) {
-    console.log(x);
-} 
-
-function p(x) {
-    window.alert(x);
-}
-
-////////////
+//See README.md
 
 var express = require('express');
+var anyDB = require('any-db');
+var conn = anyDB.createConnection('sqlite3://commondenominator.db');
+
 var app = express();
 app.use(express.bodyParser()); // definitely use this feature
-
-// var anyDB = require('any-db');
-// var conn = anyDB.createConnection('sqlite3://chatroom.db');
 
 var hbs = require('hbs');
 app.set('view engine', 'html');
@@ -22,31 +13,13 @@ app.engine('html', hbs.__express);
 app.set('views', __dirname + '/templates'); // tell Express where to find templates
 
 var connect = require('connect');
-<<<<<<< HEAD
-=======
 // possibly change '/' to '/static'
->>>>>>> eef5107bea1251b2d5a234a6c72b79194183a122
 app.use(connect.static(__dirname + '/', { maxAge: 86400000 }));
 
-////////////
+///////////////////////////
+///////////////////////////
 
-// this shit used to work: oh I know 
-//app.use(express.favicon("images/favicon.ico"));
-
-////////////
-
-// 									room TEXT,								\
-// 									name TEXT,								\
-// 									body TEXT,								\
-// 									time INTEGER 							\
-// 								)");
-// messagesDB.on('end', function() {
-// 	console.log('Made messages table!');
-// });
-// // I couldn't find a cleaner way to check for table existance:
-// messagesDB.on('error', function() {});
-
-////////////
+app.get('/favicon.ico', function(request, response) { console.log('favicon thrown out'); });
 
 app.get('/',  function(request, response){
 	response.render('project.html', {});
@@ -60,11 +33,54 @@ app.get('/messages', function(request, response){
 	response.render('messages.html', {});
 });
 
-////////////
-console.log("Server listening at port 8080");
-app.listen(8080);
+app.get('/user/:uid', function(request, response){
+	console.log(request.params.uid);
+	conn.query('SELECT uname, loc, intr FROM users WHERE uid = $1', [request.params.uid]).on('row', 
+		function(row) {
+			console.log(row);
+			
+			response.writeHead(200, {'Content-Type': 'text/html'});
+			response.write('<title>Details for ' + row.uname + '</title><h2>What is ' + row.uname + ' interested in?</h2><p>' 
+			+ row.uname + ' likes interests numbers ' + row.intr + '.</p>'
+			+ '<h2>Where is ' + row.uname + '?</h2><p>' + row.uname + ' is at ' + row.loc 
+			+ '.</p>'); 
+			response.end();
+				}); //how to access the results?
+});
 
+//note: autoincrement seems to put the first record's value at 2? how odd
+app.get('/interest/:iid', function(request, response){
+	console.log(request.params.iid);
+	conn.query('SELECT name, desc FROM interest WHERE intid = $1', [request.params.iid]).on('row', 
+		function(row) {
+			console.log(row);
+			
+			response.writeHead(200, {'Content-Type': 'text/html'});
+			response.write('<title>' + row.name + '</title><h1>' + row.name + ": " + row.desc //+ '.</h1>');
+			+ '.</h1><h2>Who likes it?</h2><ul>');
+			var likes = '';
+			conn.query('SELECT uname, users.uid FROM users, intmemb WHERE users.uid = intmemb.uid AND intmemb.intid = $1', [request.params.iid]).on('row', function(row) {
+				console.log(row);
+				likes = likes + '<li><a href="/user/' + row.uid + '">' + row.uname + '</a></li>';
+			
+			}).on('end', function() {
+			
+			
+				console.log('likes are: ' + likes)
+				response.write(likes);
+				response.write('<p>see other interests: <a href="/interest/1">Indie Music</a> | <a href="/interest/2">Computers</a> | <a href="/interest/3">Modern Art</a> | <a href="/interest/4">Surfing</a> | <a href="/interest/5">Traveling</a> | <a href="/interest/6">Concerts</a> | <a href="/interest/6">Hiking</a> | <a href="/interest/8">Chocolate</a></p>');
+				response.end('</ul>');
+				});
+			
+			
+			});
+		});
+		
+app.get('/*', function(request, response) {
+		response.writeHead(200, {'Content-Type': 'text/html'});
+		response.write('<h1>Whoa there!</h1><p>Page not found. Check the URL maybe?</p>');
+		response.end();
+		});
 
-
-
-
+//Visit localhost:8080
+app.listen(8080, function() { console.log(' - listening on port 8080');});
