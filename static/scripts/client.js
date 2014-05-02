0d94d85f7964c4d6ba968fae3178ff891698fb19
Gadi Cohen <gc21@cs.brown.edu>
2014-05-02 15:56:38 -0400

    Changed CSS, prohect.html, interest.html

--- static/scripts/client.js
+++ static/scripts/client.js
@@ -12,6 +12,19 @@ the number of users, to see how it will be viewed on
 the html page
 */
 
+$(document).ready(function(){
+	console.log($(window).scrollTop());
+	if ($(window).scrollTop()>50){
+		$('#denominatorHead').css("color", "white");
+	}
+	$('#profile-picture').click(function(){
+			console.log($(window).scrollTop());
+	});
+	$('#contact-li').one("click",function(){
+		$('#footer-p').append('<p id="email_us">Email us at firstName_lastName@brown.edu</p>');
+	});
+});
+
 
 function getUsers(numusers){
 	var table = document.getElementById('users_table');
@@ -35,14 +48,12 @@ function getUsers(numusers){
 	myimages[1]="http://widopublishing.com/wp-content/uploads/2013/01/Square-Profile-pic-2011edit.jpg";
 	myimages[2]="http://ukseds.org/wp-content/uploads/2010/02/360-square-profile-left.png";
 	myimages[3]="http://aukabusiness.com/wp-content/uploads/2013/09/Square-Profile-Pic.jpg";
-	myimages[4]="http://www.rubix.co/wp-content/uploads/2013/10/WM-SQUARE-PROFILE-PHOTO-BLK-10_10_13.jpg";
-	myimages[5]="http://ethenrichardson.com/wp-content/uploads/2013/08/Ethen-square-profile.jpg";
-	myimages[6]="http://images.coplusk.net/users/130710/avatar/large_square_profile.jpg";
-	myimages[7]="http://burrilldigitalhealth.com/wp-content/uploads/bw_square_profile_picture_1800x1800_medium_res.jpeg";
-	myimages[8]="http://greatist.com/sites/default/files/styles/square_profile/public/Profile%20Pic%20Square.jpg?itok=3q_N78z9";
-	myimages[9]="http://image3.redbull.com/rbcom/010/2012-07-08/1331574883509_1/0010/1/1500/1500/1/nasser-al-attiyah-profile-square.jpg";
-	myimages[10]="https://pbs.twimg.com/profile_images/2190908500/SamBillen_square-profile-pic.gif";
-	myimages[11]="https://pbs.twimg.com/profile_images/551405780/itamar_square_profile_new.jpg";
+	myimages[4]="http://ethenrichardson.com/wp-content/uploads/2013/08/Ethen-square-profile.jpg";
+	myimages[5]="http://images.coplusk.net/users/130710/avatar/large_square_profile.jpg";
+	myimages[6]="http://burrilldigitalhealth.com/wp-content/uploads/bw_square_profile_picture_1800x1800_medium_res.jpeg";
+	myimages[7]="http://greatist.com/sites/default/files/styles/square_profile/public/Profile%20Pic%20Square.jpg?itok=3q_N78z9";
+	myimages[8]="http://image3.redbull.com/rbcom/010/2012-07-08/1331574883509_1/0010/1/1500/1500/1/nasser-al-attiyah-profile-square.jpg";
+	myimages[9]="https://pbs.twimg.com/profile_images/2190908500/SamBillen_square-profile-pic.gif";
 
 
 	ry=Math.floor(Math.random()*myimages.length);
@@ -71,3 +82,4 @@ function getUsers(numusers){
 
 	}
 }
+
